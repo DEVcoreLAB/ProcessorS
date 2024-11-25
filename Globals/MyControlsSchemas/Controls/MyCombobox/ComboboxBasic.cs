f659633e8cd697dd4df18cd295560f7f7619a20b
Kris Kris <kwd513a@outlook.com>
2024-11-25 20:03:19 +0100

    / fonsize selected value chnaged behave

---
+++ Globals/MyControlsSchemas/Controls/MyCombobox/ComboboxBasic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Globals.MyControlsSchemas.Controls.MyCombobox
+{
+    public class ComboboxBasic : ISetControl
+    {
+        public Style SetControlProperties(double fontSize)
+        {
+            Style comboboxStyle = new Style(typeof(System.Windows.Controls.ComboBox));
+
+            comboboxStyle.Setters.Add(new Setter(Control.FontSizeProperty, fontSize));
+
+           return comboboxStyle;
+        }
+    }
+}
--- WindowFirstStart/Model/Reactors/Behaviours/FontSizesVM/FontSizeSelectedValueBehave.cs
+++ WindowFirstStart/Model/Reactors/Behaviours/FontSizesVM/FontSizeSelectedValueBehave.cs
@@ -1,10 +1,14 @@
 ﻿using Globals.Model.Observer.Components;
+using Globals.MyControlsSchemas;
+using Globals.MyControlsSchemas.Controls.MyButton;
+using Globals.MyControlsSchemas.Controls.MyCombobox;
 using Globals.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WindowFirstStart.ViewModel;
 
 namespace WindowFirstStart.Model.Reactors.Behaviours.FontSizesVM
@@ -17,10 +21,15 @@ namespace WindowFirstStart.Model.Reactors.Behaviours.FontSizesVM
 
             return new Action(() =>
             {
-                //if (mainViewModel.FontSizeSelectedValue == 18)
-                //{
-                //    mainViewModel.FontSizeSelectedValue = 12;
-                //}
+                SetCustmControl setCustmButtonControl = new SetCustmControl(new ButtonBasic());
+                Style buttonStyle = setCustmButtonControl.setControl.SetControlProperties(mainViewModel.FontSizeSelectedValue);
+                Application.Current.Resources.Remove(typeof(System.Windows.Controls.Button));
+                Application.Current.Resources.Add(typeof(System.Windows.Controls.Button), buttonStyle);
+
+                SetCustmControl setCustmComboboxControl = new SetCustmControl(new ComboboxBasic());
+                Style comboboxStyle = setCustmComboboxControl.setControl.SetControlProperties(mainViewModel.FontSizeSelectedValue);
+                Application.Current.Resources.Remove(typeof(System.Windows.Controls.ComboBox));
+                Application.Current.Resources.Add(typeof(System.Windows.Controls.ComboBox), comboboxStyle);
             });
         }
     }
