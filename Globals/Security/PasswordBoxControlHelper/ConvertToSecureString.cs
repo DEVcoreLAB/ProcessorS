cab7bf7f1d24d97356b1c2120c314e51bc483564
Kris Kris <kwd513a@outlook.com>
2024-12-11 20:04:59 +0100

    + ConvertToSecureString in globals

---
+++ Globals/Security/PasswordBoxControlHelper/ConvertToSecureString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globals.Security.PasswordBoxControlHelper
+{
+    public class ConvertToSecureString
+    {
+        public static SecureString Convert(string plainText)
+        {
+            if (plainText == null)
+            {
+                return null;
+            }
+
+            SecureString secure = new SecureString();
+
+            foreach (char c in plainText)
+            {
+                secure.AppendChar(c);
+            }
+
+            secure.MakeReadOnly();
+
+            return secure;
+        }
+    }
+}
