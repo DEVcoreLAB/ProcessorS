12b5d7045c0535ddfa18f775ef1c07dd315296eb
Kris Kris <kwd513a@outlook.com>
2024-12-13 21:07:17 +0100

    / few code lines for prepare to save settings + PassKey +SaveToFileSecuredStringToString

---
+++ Globals/Security/PasswordBoxControlHelper/SaveToFileSecuredStringToString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globals.Security.PasswordBoxControlHelper
+{
+    public class SaveToFileSecuredStringToString
+    {
+       public static string Save(SecureString secureString)
+       {
+            return Marshal.PtrToStringBSTR(Marshal.SecureStringToBSTR(secureString));
+       }
+    }
+}
