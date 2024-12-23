499f4b379632e95de68106fd352aa26d712dbcb4
Kris Kris <kwd513a@outlook.com>
2024-12-23 20:08:52 +0100

    + help FlowDocumentReader + license FlowDocumentReader

---
+++ WindowFirstStart/ViewModel/HelpSection.cs
@@ -0,0 +1,25 @@
+﻿using Globals.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace WindowFirstStart.ViewModel
+{
+    internal partial class MainViewModel : BaseViewModel
+    {
+		private FlowDocument helpFlowDocument;
+		public FlowDocument HelpFlowDocument
+		{
+			get { return helpFlowDocument; }
+			set 
+			{ 
+				helpFlowDocument = value;
+				OnPropertyChanged(nameof(HelpFlowDocument));
+			}
+		}
+
+	}
+}
