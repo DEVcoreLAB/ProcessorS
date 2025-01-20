21033f615fe4d414d45dd484bd905611773eefdb
Kris Kris <kwd513a@outlook.com>
2025-01-21 00:55:01 +0100

    + show start bar command

---
+++ WindowMainWindowContextMenu/ViewModel/Command/ShowStartBarCommand/ShowStartBarAction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Effects;
+using WindowMainWindowContextMenu.View;
+using Windows.UI;
+
+namespace WindowMainWindowContextMenu.ViewModel.Command.ShowStartBarCommand
+{
+    internal class ShowStartBarAction
+    {
+        MainViewModel mainViewModel;
+
+        public ShowStartBarAction(MainViewModel mainViewModel)
+        {
+            this.mainViewModel = mainViewModel;
+        }
+
+        public void Execute(object? parameter)
+        {
+            var window = parameter as ContextMenuWindow;
+
+            if (mainViewModel.IsStartBarVisible == false)
+            {
+                window.Hide();
+
+                Application.Current.MainWindow.Effect = new BlurEffect { Radius = 0 };
+
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+                Application.Current.MainWindow.Width = SystemParameters.WorkArea.Width + 7;
+                Application.Current.MainWindow.Height = SystemParameters.WorkArea.Height + 7;
+                Application.Current.MainWindow.Left = SystemParameters.WorkArea.Left;
+                Application.Current.MainWindow.Top = SystemParameters.WorkArea.Top;
+                mainViewModel.IsStartBarVisible = true;
+            }
+            else if (mainViewModel.IsStartBarVisible == true)
+            {
+                window.Hide();
+                Application.Current.MainWindow.WindowState |= WindowState.Maximized;
+                Application.Current.MainWindow.Effect = new BlurEffect { Radius = 0 };
+                mainViewModel.IsStartBarVisible = false;
+            }
+        }
+    }
+}
--- WindowMainWindowContextMenu/ViewModel/MainViewModel.cs
+++ WindowMainWindowContextMenu/ViewModel/MainViewModel.cs
@@ -5,16 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WindowMainWindowContextMenu.Command.LockScreenCommands;
+using WindowMainWindowContextMenu.ViewModel.Command.ShowStartBarCommand;
 
 namespace WindowMainWindowContextMenu.ViewModel
 {
     internal class MainViewModel
     {
         public RelayCommand LockSreenCommand {  get; }
+        public RelayCommand ShowStartBarCommand { get; }
 
         public MainViewModel()
         {
             LockSreenCommand = new RelayCommand(new LockScreenAction().Execute,new LockScreenPredict().Check);
+
+            IsStartBarVisible = false;
+            ShowStartBarAction showStartBarAction = new ShowStartBarAction(this);
+            ShowStartBarCommand = new RelayCommand(
+                showStartBarAction.Execute,  
+                () => new ShowStartBarPredict().Check());
         }
+
+        private bool isStartBarVisible;
+        public bool IsStartBarVisible
+        {
+            get { return isStartBarVisible; }
+            set { isStartBarVisible = value; }
+        }
+
     }
 }
