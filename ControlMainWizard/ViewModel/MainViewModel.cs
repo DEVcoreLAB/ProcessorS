c6b8b061c240f359f88301425dcf330c25c12f6c
Kris Kris <kwd513a@outlook.com>
2025-01-23 10:23:57 +0100

    + main wizard main user control

---
+++ ControlMainWizard/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ControlMainWizard.ViewModel
+{
+    internal partial class MainViewModel : Globals.ViewModel.BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("view model connection test");
+        }
+    }
+}
--- WindoRegularStart/View/MainWindow/MainWindow.xaml.cs
+++ WindoRegularStart/View/MainWindow/MainWindow.xaml.cs
@@ -54,5 +54,10 @@ namespace WindowRegularStart.View.MainWindow
                new Globals.MyDialogsAndWindows.MyLockScreen.LockScreenWindow();
             lockScreenWindow.Show();
         }
+
+        private void WizardButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainViewModel.MainUserControl = new ControlMainWizard.View.WizardMainControl();
+        }
     }
 }
