bb31599a09326fe0947ab2964cec409eb3fb543e
Kris Kris <kwd513a@outlook.com>
2025-01-26 11:08:03 +0100

    + block diagram control

---
+++ SubControlBlockDiagramProject/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Globals.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubControlBlockDiagramProject.ViewModel
+{
+    internal partial class MainViewModel : BaseViewModel
+    {
+    }
+}
