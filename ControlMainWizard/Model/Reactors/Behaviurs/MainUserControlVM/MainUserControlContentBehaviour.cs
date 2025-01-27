9ba69fd3a3917ae2e88e389505a94e6fa0b89476
Kris Kris <kwd513a@outlook.com>
2025-01-27 12:04:57 +0100

    / colors of wizard buttons change by click

--- ControlMainWizard/Model/Reactors/Behaviurs/MainUserControlVM/MainUserControlContentBehaviour.cs
+++ ControlMainWizard/Model/Reactors/Behaviurs/MainUserControlVM/MainUserControlContentBehaviour.cs
@@ -4,9 +4,11 @@ using Globals.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 
 namespace ControlMainWizard.Model.Reactors.Behaviurs.MainUserControlVM
 {
@@ -18,8 +20,54 @@ namespace ControlMainWizard.Model.Reactors.Behaviurs.MainUserControlVM
 
             return new Action(() => 
             {
-                MessageBox.Show("changed");
+                string FirstPartOfChangedPropertyName = GetFirstPartOfChangedPropertyName(mainViewModel.MainUserControlContent.GetType().Name); 
+
+                var properties = typeof(T).GetProperties();
+
+
+
+                foreach (var property in properties)
+                {
+                    if (property.Name.Contains(FirstPartOfChangedPropertyName)
+                    && property.PropertyType == typeof(SolidColorBrush))
+                    {
+                        var brush = property.GetValue(viewModel) as SolidColorBrush;
+
+                        if (brush != null)
+                        {
+                            brush.Color = Colors.Orange;
+
+                            property.SetValue(viewModel, brush);
+                        }
+                    }
+                    else if (!(property.Name.Contains(FirstPartOfChangedPropertyName))
+                    && property.PropertyType == typeof(SolidColorBrush))
+                    {
+                        var brush = property.GetValue(viewModel) as SolidColorBrush;
+
+                        if (brush != null)
+                        {
+                            brush.Color = Colors.Black;
+
+                            property.SetValue(viewModel, brush);
+                        }
+                    }
+                }
             });
+        }
+
+        private string GetFirstPartOfChangedPropertyName(string fullName)
+        {
+            for (int i = 1; i < fullName.Length; i++)
+            {
+                if (char.IsUpper(fullName[i]))
+                {
+                    string first = fullName.Substring(0, i);
+                    return first;
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
