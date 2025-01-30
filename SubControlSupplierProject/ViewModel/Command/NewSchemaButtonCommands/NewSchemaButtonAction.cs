dfd39234a7c55b4c4a40bd50b940969f989170f5
Kris Kris <kwd513a@outlook.com>
2025-01-30 11:10:56 +0100

    + new suppplier schema CONTROL

--- SubControlSupplierProject/ViewModel/Command/NewSchemaButtonCommands/NewSchemaButtonAction.cs
+++ SubControlSupplierProject/ViewModel/Command/NewSchemaButtonCommands/NewSchemaButtonAction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SubControlSupplierProject.View.NewSchemaUserControl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace SubControlSupplierProject.ViewModel.Command.NewSchemaButtonCommands
 
         public void Execute(object? parameter)
         {
-            MessageBox.Show("test");   
+            mainViewModel.SupplierSchemasUserControl = new NewSchemaControl();
         }
     }
 }
