f5f189db97946816de2b33cedaaaee9eb55d6963
Kris Kris <kwd513a@outlook.com>
2025-01-30 11:20:05 +0100

    + structure of model view and mainviewmodel for supplier new schema

--- SubControlSupplierProject/View/NewSchemaUserControl/NewSchemaControl.xaml.cs
+++ SubControlSupplierProject/View/NewSchemaUserControl/NewSchemaControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SubControlSupplierProject.ViewModel.NewSchemaUserControl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,13 @@ namespace SubControlSupplierProject.View.NewSchemaUserControl
     /// </summary>
     public partial class NewSchemaControl : UserControl
     {
+        NewSchemaMainViewModel NewSchemaMainViewModel { get; }
+
         public NewSchemaControl()
         {
             InitializeComponent();
+            NewSchemaMainViewModel = new NewSchemaMainViewModel();
+            this.DataContext = NewSchemaMainViewModel;
         }
     }
 }
