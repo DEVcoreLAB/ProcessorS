ae318798e4e473b91dd1b6d3abc75f813ae9d208
Kris Kris <kwd513a@outlook.com>
2025-02-16 16:36:26 +0100

    / save new supplier updt

--- Globals/DbOperations/Wizard/ItemViaSchema/SaveToTable.cs
+++ Globals/DbOperations/Wizard/ItemViaSchema/SaveToTable.cs
@@ -1,5 +1,6 @@
 ﻿using Globals.DbOperations.DbasesNames;
 using Globals.Logger.Log4N;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace Globals.DbOperations.Wizard.TableViaSchema
 {
@@ -19,14 +21,14 @@ namespace Globals.DbOperations.Wizard.TableViaSchema
         public SaveToTable(Type dataSchema, object dataObject)
         {
             _dataSchema = dataSchema;
-            _dataObject = dataObject; 
+            _dataObject = dataObject;
         }
 
         public async Task Save(string connectionString, DbNames nameOfDataBase, string nameOfTable)
         {
             try
             {
-               if (_dataSchema == null || _dataObject == null)
+                if (_dataSchema == null || _dataObject == null)
                 {
                     await Application.Current.Dispatcher.InvokeAsync(() =>
                     {
@@ -38,33 +40,157 @@ namespace Globals.DbOperations.Wizard.TableViaSchema
                     return;
                 }
 
-                StringBuilder stringBuilder = new StringBuilder();
-                var propertyInfo = _dataSchema.GetProperties(); 
+                var propertyInfo = _dataSchema.GetProperties();
 
-                foreach (var property in propertyInfo)
+                await using (var connection = new SqlConnection(connectionString))
                 {
-                    object propertyValue = property.GetValue(_dataObject); 
-                    if (propertyValue is ObservableCollection<string> collection)
+                    await connection.OpenAsync();
+
+                    // switch to correct database
+                    await using (var cmd = new SqlCommand($"USE [{nameOfDataBase.ToString()}]", connection))
                     {
-                        stringBuilder.AppendLine($@"List: {property.Name}");
-                        foreach (var item in collection)
-                        {
-                            await Task.Delay(1);
-                            stringBuilder.AppendLine($@"      {item}");
-                        }
-                        stringBuilder.Append("\n");
+                        await cmd.ExecuteNonQueryAsync();
                     }
-                    else
+
+                    // create main datatable if not exist
+                    string createMainTableQuery = $@"
+                    IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = '{nameOfTable}')
+                    BEGIN
+                    CREATE TABLE [{nameOfTable}] (
+                    ID INT IDENTITY(1,1) PRIMARY KEY,
+                    PropertyName NVARCHAR(256) NOT NULL,
+                    DataType NVARCHAR(256) NOT NULL,
+                    BoolValue BIT NULL,
+                    Note NVARCHAR(256) NULL,
+                    ListGroupID INT NULL
+                    )
+                    END";
+
+                    await using (var cmd = new SqlCommand(createMainTableQuery, connection))
                     {
-                        stringBuilder.AppendLine($@"{property.Name}   {property.PropertyType}  {propertyValue}");
+                        await cmd.ExecuteNonQueryAsync();
+                    }
+
+                    foreach (var property in propertyInfo)
+                    {
+                        // processing all propeties of _dataschemaObject
+
+                        #region observableCollection<string>Type
+                        if (property.PropertyType == typeof(ObservableCollection<string>))
+                        {
+                            // name of additional table NameOfTable_NameOfProperty
+                            string listTableName = $"{nameOfTable}_{property.Name}";
+
+                            // create this additional table
+                            string createListTableQuery = $@"
+                                    IF NOT EXISTS 
+                                    (SELECT * FROM sys.tables WHERE name = '{listTableName}')
+                                    BEGIN
+                                    CREATE TABLE [{listTableName}] (
+                                    ID INT IDENTITY(1,1) PRIMARY KEY,
+                                    ListGroupID INT NOT NULL,
+                                    ListItem NVARCHAR(256) NOT NULL
+                                    )
+                                    END";
+
+                            await using (var cmd = new SqlCommand(createListTableQuery, connection))
+                            {
+                                await cmd.ExecuteNonQueryAsync();
+                            }
+
+                            //  insert a row into the main table and retrieve the generated ID
+                            string insertMainListQuery = $@"
+                                    INSERT INTO [{nameOfTable}] 
+                                    (PropertyName, DataType, BoolValue, Note, ListGroupID)
+                                    VALUES
+                                    (@propName, @dataType, NULL, NULL, NULL);
+                                    SELECT SCOPE_IDENTITY();";
+
+                            int mainId;
+                            await using (var cmd = new SqlCommand(insertMainListQuery, connection))
+                            {
+                                cmd.Parameters.AddWithValue("@propName", property.Name);
+                                cmd.Parameters.AddWithValue("@dataType", "List<string>");
+                                mainId = Convert.ToInt32(cmd.ExecuteScalar());
+                            }
+
+                            // update a row in the main table by setting ListGroupID to the value of the primary key
+                            string updateMainQuery = $@"
+                                    UPDATE [{nameOfDataBase}].dbo.[{nameOfTable}]
+                                    SET ListGroupID = @mainId
+                                    WHERE ID = @mainId";
+
+                            await using (var cmd = new SqlCommand(updateMainQuery, connection))
+                            {
+                                cmd.Parameters.AddWithValue("@mainId", mainId);
+                                await cmd.ExecuteNonQueryAsync();
+                            }
+
+                            // retrieve the list items and, for each one, insert a row into the additional table
+                            ObservableCollection<string> listValues = property.GetValue(_dataObject) as ObservableCollection<string>;
+                            if (listValues != null)
+                            {
+                                foreach (var item in listValues)
+                                {
+                                    string insertListItemQuery = $@"
+                                            INSERT INTO [{listTableName}] 
+                                            (ListGroupID, ListItem)
+                                            VALUES
+                                            (@groupId, @listItem)";
+
+                                    await using (var cmd = new SqlCommand(insertListItemQuery, connection))
+                                    {
+                                        cmd.Parameters.AddWithValue("@groupId", mainId);
+                                        cmd.Parameters.AddWithValue("@listItem", item);
+                                        await cmd.ExecuteNonQueryAsync();
+                                    }
+                                }
+                            }
+                        }
+                        #endregion
+
+                        #region booleanType
+                        else if (property.PropertyType == typeof(bool))
+                        {
+                            bool boolValue = (bool)property.GetValue(_dataObject);
+                            string insertBoolQuery = $@"
+                            INSERT INTO [{nameOfTable}] 
+                            (PropertyName, DataType, BoolValue, Note, ListGroupID)
+                            VALUES
+                            (@propName, @dataType, @boolValue, NULL, NULL)";
+
+                            await using (var cmd = new SqlCommand(insertBoolQuery, connection))
+                            {
+                                cmd.Parameters.AddWithValue("@propName", property.Name);
+                                cmd.Parameters.AddWithValue("@dataType", "bool");
+                                cmd.Parameters.AddWithValue("@boolValue", boolValue);
+                                await cmd.ExecuteNonQueryAsync();
+                            }
+                        }
+                        #endregion
+
+                        #region stringType
+                        else if (property.PropertyType == typeof(string))
+                        {
+                            string stringValue = (string)property.GetValue(_dataObject);
+                            string insertBoolQuery = $@"
+                            INSERT INTO [{nameOfTable}] 
+                            (PropertyName, DataType, BoolValue, Note, ListGroupID)
+                            VALUES
+                            (@propName, @dataType, NULL, @Note, NULL)";
+
+                            await using (var cmd = new SqlCommand(insertBoolQuery, connection))
+                            {
+                                cmd.Parameters.AddWithValue("@propName", property.Name);
+                                cmd.Parameters.AddWithValue("@dataType", "string");
+                                cmd.Parameters.AddWithValue("@Note", stringValue);
+                                await cmd.ExecuteNonQueryAsync();
+                            }
+                        }
+                        #endregion
                     }
                 }
 
-                await Application.Current.Dispatcher.InvokeAsync(() =>
-                {
-                    MessageBox.Show(stringBuilder.ToString(), "data to save info",
-                                    MessageBoxButton.OK, MessageBoxImage.Information);
-                });
             }
             catch (Exception ex)
             {
